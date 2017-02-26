922ab890d4e0d1fb314f3b03783d31122ff7644f
sepehr <sepehr@eduroam-169-233-214-120.ucsc.edu>
2017-02-25 16:00:24 -0800

    update

--- LAB4/LAB4.asm
+++ LAB4/LAB4.asm
@@ -23,17 +23,17 @@ GET_INPUT
 	BRz GET_CIPHER	; if char == LF
 	LD R1,D
 	ADD R1,R0,R1
-	BRz FLAG_D
+	BRz FLAG_D 		; if char == D
 	LD R1,E
 	ADD R1,R0,R1
-	BRz FLAG_E
+	BRz FLAG_E 		; if char == E
 	LD R1,X
 	ADD R1,R0,R1
-	BRz FLAG_X
+	BRz FLAG_X 		; if char == X
 	BRnp GET_INPUT	;if user entered some other letters, loop back
 GET_CIPHER
 	LD R1,FLAG
-	BRn EXIT_PROGRAM ; if flag = -1
+	BRn EXIT_PROGRAM ; if flag = -1: exit
 	LEA R0,CIPHER_KEY
 	PUTS
 CIPHER_INPUT
@@ -58,41 +58,46 @@ MULT_LOOP
 	AND R3,R3,0 	; reset to zero
 	BRnzp CIPHER_INPUT
 
-D_E
-	AND R4,R4,0		; REINIT
-	AND R3,R3,0		; REINIT
-	AND R2,R2,0		; REINIT
-	AND R1,R1,0		; REINIT
-	LEA R2,ARRAY
+D_E 
+	AND R4,R4,0			; REINIT
+	AND R3,R3,0			; REINIT
+	AND R2,R2,0			; REINIT 
+	AND R1,R1,0			; REINIT
+	LEA R2,ARRAY 		; ARRAY[I]
+	LEA R3,ARRAY 	    ; ARRAY[I+200]
+	ADD R3,R3,DE_ARRAY  ; ARRAY[200];
 	LD R1,FLAG
-	;BRn EXIT_PROGRAM
-	BRz GET_D_STRING
-	BRp GET_E_STRING
+	BRn EXIT_PROGRAM	; if flag = -1
+	BRz GET_D_STRING	; if flag = 0
+	BRp GET_E_STRING	; if flag = 1
 
 EXIT_PROGRAM
 	LEA R0,ARRAY
-	OUT
+	PUTS
 	LD R0,LF
 	NOT R0,R0
 	ADD R0,R0,1
 	OUT
-
-
 
 HALT
 
 GET_E_STRING
 	GETC
 	OUT
-	STR R0,R2,0 	;ARRAY[i] = char
+	STR R0,R2,0 	; ARRAY[i] = char
+	JSR DECRYPT_CHAR 
 	ADD R2,R2,1 	; ARRAY++
 	LD R1,LF		; R1 = '\n'
 	ADD R1,R0,R1 	; if char == '\n'
-	BRp GET_E_STRING; IF LF EXIT
+	BRp GET_E_STRING; IF LF EXIT else get input
 	AND R0,R0,0
-	STR R0,R2,0
+	STR R0,R2,0     ; ARRAY[i]='\0'
+	STR R0,R3,0  	; ARRAY[200+I] = '\0'
 	BRz EXIT_PROGRAM
 GET_D_STRING
+	GETC
+	OUT
+	STR R0,R2,0 	; ARRAY[i] = char
 	BR EXIT_PROGRAM
 FLAG_D
 	ADD R1,R1,1
@@ -109,6 +114,15 @@ FLAG_X
 	ST R1,FLAG
 	AND R1,R1,0
 	BRz GET_INPUT
+DECRYPT_CHAR
+	LD R4,CIPHER
+	NOT R4,R4           ; CIPHER * -1
+	ADD R4,R0,CIPHER 	; ARRAY[I]CHAR - CIPHER
+	STR R4,R3,0    		; 
+	ADD R3,R3,1			; ARRAY[200]++
+	RET
+
+
 GREETING	.STRINGZ "Hello, welcome to my Caesar Cipher program\n"
 OPTIONS		.STRINGZ "Do you want to (E)ncrypt or (D)ecrypt or e(X)it?\n"
 CIPHER_KEY 	.STRINGZ "What is the cipher (1-25)?\n"
@@ -121,6 +135,6 @@ ZERO 		.FILL -48
 CIPHER  	.FILL 0
 DIGIT 		.FILL 0
 ARRAY 		.BLKW 400
-DE_ARRAY    .FILL 201
+DE_ARRAY    .FILL 200
 
 .END
