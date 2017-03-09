6a010aee45c92106fabe313a54c072532f5f5f25
Sepehr <sepehrtheraiss@gmail.com>
2017-03-09 13:32:17 -0800

    update

--- LAB5/Lab5Part1.s
+++ LAB5/Lab5Part1.s
@@ -9,6 +9,18 @@
 
 .ent main
 main:
+    /* this code blocks sets up the ability to print, do not alter it */
+    ADDIU $v0,$zero,1
+    LA $t0,__XC_UART
+    SW $v0,0($t0)
+    LA $t0,U1MODE
+    LW $v0,0($t0)
+    ORI $v0,$v0,0b1000
+    SW $v0,0($t0)
+    LA $t0,U1BRG
+    ADDIU $v0,$zero,12
+    SW $v0,0($t0)
+    
     # set LED's as output:
     LA $t1, TRISE	   # int32* t1= &TRISE; //load TRISE address 
     LI $t2, 0		   # int32 t2 = 0;
@@ -34,32 +46,39 @@ main:
     # then put the bit in the desired led position 
     Loop:  LW $t1,PORTD     # value of the input button    
 	   ANDI $t2,$t1,0b100000 # mask for button bit 5
-	   SRL $t2,$t2,4    # t2 >> 4; // shift the value of t2, 4 to right
-	   SW $t2,0($t3)    # *t3 = t2; // store the value of t2 to PORTE
-	   ANDI $t2,$t1,0b1000000 # mask for button bit 6
-	   SRL $t2,$t2,4    # t2 >> 4; // shift the value of t2, 4 to right
+	   # SRL $t2,$t2,4    # t2 >> 4; // shift the value of t2, 4 to right
+	   SW $t2,0($t3)    # *t3 = t2; // store the value of t2 to PORTE, LED ON
+	   ANDI $t2,$t1,0b1000000 # mask for button bit 6, if 0 does nothing
+	   # SRL $t2,$t2,4    # t2 >> 4; // shift the value of t2, 4 to right
 	   SW $t2,0($t3)    # *t3 = t2; // store the value of t2 to PORTE
 	   ANDI $t2,$t1,0b10000000 # mask for button bit 7
-	   SRL $t2,$t2,4    # t2 >> 4; // shift the value of t2, 4 to right
+	   # SRL $t2,$t2,4    # t2 >> 4; // shift the value of t2, 4 to right
 	   SW $t2,0($t3)    # *t3 = t2; // store the value of t2 to PORTE
 	   ANDI $t2,$t1,0b100000000 # mask for button bit 8
-	   SRL $t2,$t2,4    # t2 >> 4; // shift the value of t2, 4 to right
+	   SRL $t2,$t2,8    # t2 >> 8; // shift the value of t2, 4 to right
 	   SW $t2,0($t3)    # *t3 = t2; // store the value of t2 to PORTE
 	   ANDI $t2,$t1,0b1000000000 # mask for button bit 9
-	   SRL $t2,$t2,4    # t2 >> 4; // shift the value of t2, 4 to right
+	   SRL $t2,$t2,8    # t2 >> 8; // shift the value of t2, 4 to right
 	   SW $t2,0($t3)    # *t3 = t2; // store the value of t2 to PORTE
 	   ANDI $t2,$t1,0b10000000000 # mask for button bit 10
-	   SRL $t2,$t2,4    # t2 >> 4; // shift the value of t2, 4 to right
+	   SRL $t2,$t2,8    # t2 >> 8; // shift the value of t2, 4 to right
 	   SW $t2,0($t3)    # *t3 = t2; // store the value of t2 to PORTE
 	   ANDI $t2,$t1,0b100000000000 # mask for button bit 11
-	   SRL $t2,$t2,4    # t2 >> 4; // shift the value of t2, 4 to right
+	   SRL $t2,$t2,8    # t2 >> 8; // shift the value of t2, 4 to right
 	   SW $t2,0($t3)    # *t3 = t2; // store the value of t2 to PORTE
 	   LW $t1,PORTF
 	   ANDI $t2,$t1,0b10 # mask for button bit 1
-	   SRL $t2,$t2,1    # t2 >> 1; // shift the value of t2, 4 to right
+	   SLL $t2,$t2,3    # t2 << 3; // shift the value of t2, 4 to right
 	   SW $t2,0($t3)    # *t3 = t1; // store the value of t2 to PORTE
 	   J Loop
 NOP
 .end main
+hmm:    J hmm
+    NOP
+endProgram:
+    J endProgram
+    NOP
+.data    
+HelloWorld: .asciiz "Assembly Hello World \n"
 
 
--- LAB5/LabPart2.s
+++ LAB5/LabPart2.s
@@ -9,42 +9,93 @@
 
 .ent main
 main:
+    /* this code blocks sets up the ability to print, do not alter it */
+    ADDIU $v0,$zero,1
+    LA $t0,__XC_UART
+    SW $v0,0($t0)
+    LA $t0,U1MODE
+    LW $v0,0($t0)
+    ORI $v0,$v0,0b1000
+    SW $v0,0($t0)
+    LA $t0,U1BRG
+    ADDIU $v0,$zero,12
+    SW $v0,0($t0)
+    /* your code goes underneath this */
+
+    LA $a0,HelloWorld
+    JAL puts
+    NOP
+    
+    # set buttons and switches to input
+    LA $t1, TRISD	   # int32* t1= &TRISD; //load PORTE address 
+    LI $t2, 0xFFFF	  
+    SW $t2, 0($t1)         # *t1 = 11111111; // set switches as input
     # set LED's as output:
     LA $t1, TRISE	   # int32* t1= &TRISE; //load TRISE address 
     LI $t2, 0		   # int32 t2 = 0;
     SW $t2, 0($t1)         # *t1 = 0x0000; // set TRISE as output
     # turn leds off
     LA $t3, PORTE          # int32* t3 = &PORTE; //load PORTE address 
-    LI $t5, 0b0		   # int32 t4 = 0; 
-    SW $t5, 0($t3)	   # *t3 = 4; // set TRISE to 0 to turn all LEDS off 
-    LI $t6,0b10000000	   # counter 8 
+    SW $0, 0($t3)	   # *t3 = 4; // set TRISE to 0 to turn all LEDS off 
+    LI $t6,0b10000000	   # counter 8
+    LI $t1, 1
     LI $t4, 0b1
     # loop to turn led 1-8 on
-    Loop:
- 	#BEQ $t4,$t6,Set_Counter_to_1 
+    Loop1:
+	# get users input from switches
+	LW $a0,PORTD     # value of the input button 
+	ANDI $a0,$a0,0b111100000000 # mask for switches bit 8-11,
+	SRL $a0,$a0,8    # t2 >> 8; // shift the value of t2, 4 to right
+	ANDI $a0,$a0,0b1111 # mask for only desired digits double checking
+	ADD $a0,$a0,1
+	JAL Calc # calculate the speed of the light
+	NOP 
+ 	BEQ $t4,$t6,Loop2 # if t4 is 8, so the last LED turns on too
 	SW $t4,0($t3)	   # turn led[i] on
+	SLL $t4,$t4,1    # t2 << 1; // shift the value of t2, 4 to right
 	JAL Waste_Time
 	NOP
-	SW $t5,0($t3)	   # turn led[i] off
-	#NOP
+	J Loop1
+	NOP
+    # loop to turn led 8-1 on
+    Loop2:
+    	# get users input from switches
+	LW $a0,PORTD     # value of the input button 
+	ANDI $a0,$a0,0b111100000000 # mask for switches bit 8-11,
+	SRL $a0,$a0,8    # t2 >> 8; // shift the value of t2, 4 to right
+	ANDI $a0,$a0,0b1111 # mask for only desired digits double checking
+	ADD $a0,$a0,1
+	JAL Calc # calculate the speed of the light
+	NOP 
+ 	BEQ $t4,$t1,Loop1 # if t4 is 1, so the last LED turns on too
+	SW $t4,0($t3)	   # turn led[i] on
+	SRL $t4,$t4,1    # t2 >> 1; // shift the value of t2, 4 to right
 	JAL Waste_Time
 	NOP
-	J Loop
-	NOP
-Set_Counter_to_1:
-    LI $t4,1
-    J Loop
+	J Loop2
+	NOP	
+Calc:
+    LW $t5, counter
+    MUL $t5,$t5,$a0
+    JR $31
     NOP
 Waste_Time:
-    LI $t7,10000
+    ADD $t7,$t5,$0 # copy t5 which is the calculated light speed
     Start_Loop:
 	SUBU $t7,$t7,1
 	BGTZ $t7,Start_Loop
 	 NOP
     JR $31
      NOP
-    # 1000000 fukcing counter that actually actually works!
-    # 10000
 .end main
+    
+hmm:    J hmm
+    NOP
+endProgram:
+    J endProgram
+    NOP
+.data    
+HelloWorld: .asciiz "Assembly Hello World \n"
+counter: .word 10000
     
     
