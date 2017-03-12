3fd3ed2493b9168b212667cc812fd6e6d7fe6aac
Sepehr <sepehrtheraiss@gmail.com>
2017-03-11 23:34:33 -0800

    new files

--- LAB5/Lab5Part1.s
+++ LAB5/Lab5Part1.s
@@ -1,3 +1,9 @@
+/*
+ *  Sepehr Raissian
+ *  CMPE 12
+ *  LAB 5 part 1
+ *  SECTION 1D 
+ */
 #include <WProgram.h>
 #include <xc.h>
 /* define all global symbols here */
@@ -21,6 +27,12 @@ main:
     ADDIU $v0,$zero,12
     SW $v0,0($t0)
     
+    /* your code goes underneath this */
+
+    LA $a0,HelloWorld
+    JAL puts
+    NOP
+    
     # set LED's as output:
     LA $t1, TRISE	   # int32* t1= &TRISE; //load TRISE address 
     LI $t2, 0		   # int32 t2 = 0;
@@ -28,7 +40,7 @@ main:
     # turn leds off
     LA $t3, PORTE          # int32* t3 = &PORTE; //load PORTE address 
     LI $t4, 0b0		   # int32 t4 = 0; 
-    SW $t4, 0($t3)	   # *t3 = 4; // set TRISE to 0 to turn all LEDS off 
+    SW $t4, 0($t3)	   # *t3 = 4; // set PORTE to 0 to turn all LEDS off 
     
     # BUTTON
     # set Button 1 as input:
@@ -42,34 +54,34 @@ main:
     
     # get buttons input state
     # mask to check only for desired inputs 
-    # shift to the current location 
+    # shift to the current location of led
     # then put the bit in the desired led position 
     Loop:  LW $t1,PORTD     # value of the input button    
-	   ANDI $t2,$t1,0b100000 # mask for button bit 5
+	   ANDI $t2,$t1,0b100000 # mask for button bit 5, if 0 does nothing
 	   # SRL $t2,$t2,4    # t2 >> 4; // shift the value of t2, 4 to right
-	   SW $t2,0($t3)    # *t3 = t2; // store the value of t2 to PORTE, LED ON
+	   SW $t2,0($t3)    # *t3 = t2; // store the value of t2 to PORTE, LED 5 ON 
 	   ANDI $t2,$t1,0b1000000 # mask for button bit 6, if 0 does nothing
 	   # SRL $t2,$t2,4    # t2 >> 4; // shift the value of t2, 4 to right
-	   SW $t2,0($t3)    # *t3 = t2; // store the value of t2 to PORTE
+	   SW $t2,0($t3)    # *t3 = t2; // store the value of t2 to PORTE, LED 6 ON 
 	   ANDI $t2,$t1,0b10000000 # mask for button bit 7
 	   # SRL $t2,$t2,4    # t2 >> 4; // shift the value of t2, 4 to right
-	   SW $t2,0($t3)    # *t3 = t2; // store the value of t2 to PORTE
+	   SW $t2,0($t3)    # *t3 = t2; // store the value of t2 to PORTE, LED 7 ON 
 	   ANDI $t2,$t1,0b100000000 # mask for button bit 8
 	   SRL $t2,$t2,8    # t2 >> 8; // shift the value of t2, 4 to right
-	   SW $t2,0($t3)    # *t3 = t2; // store the value of t2 to PORTE
+	   SW $t2,0($t3)    # *t3 = t2; // store the value of t2 to PORTE, LED 8 ON 
 	   ANDI $t2,$t1,0b1000000000 # mask for button bit 9
 	   SRL $t2,$t2,8    # t2 >> 8; // shift the value of t2, 4 to right
-	   SW $t2,0($t3)    # *t3 = t2; // store the value of t2 to PORTE
+	   SW $t2,0($t3)    # *t3 = t2; // store the value of t2 to PORTE, LED 1 ON 
 	   ANDI $t2,$t1,0b10000000000 # mask for button bit 10
 	   SRL $t2,$t2,8    # t2 >> 8; // shift the value of t2, 4 to right
-	   SW $t2,0($t3)    # *t3 = t2; // store the value of t2 to PORTE
+	   SW $t2,0($t3)    # *t3 = t2; // store the value of t2 to PORTE, LED 2 ON 
 	   ANDI $t2,$t1,0b100000000000 # mask for button bit 11
 	   SRL $t2,$t2,8    # t2 >> 8; // shift the value of t2, 4 to right
-	   SW $t2,0($t3)    # *t3 = t2; // store the value of t2 to PORTE
+	   SW $t2,0($t3)    # *t3 = t2; // store the value of t2 to PORTE, LED 3 ON 
 	   LW $t1,PORTF
-	   ANDI $t2,$t1,0b10 # mask for button bit 1
-	   SLL $t2,$t2,3    # t2 << 3; // shift the value of t2, 4 to right
-	   SW $t2,0($t3)    # *t3 = t1; // store the value of t2 to PORTE
+	   ANDI $t2,$t1,0b10 # mask for button bit 1    
+	   SLL $t2,$t2,3    # t2 << 3; // shift the value of t2, 3 to right
+	   SW $t2,0($t3)    # *t3 = t1; // store the value of t2 to PORTE to turn led 4 on
 	   J Loop
 NOP
 .end main
--- LAB5/Lab5Part2.s
+++ LAB5/Lab5Part2.s
@@ -1,3 +1,9 @@
+/*
+ *  Sepehr Raissian
+ *  CMPE 12
+ *  LAB 5 part 2
+ *  SECTION 1D 
+ */
 #include <WProgram.h>
 #include <xc.h>
 /* define all global symbols here */
@@ -28,7 +34,7 @@ main:
     
     # set buttons and switches to input
     LA $t1, TRISD	   # int32* t1= &TRISD; //load PORTE address 
-    LI $t2, 0xFFFF	  
+    LI $t2, 0xFFFF	    
     SW $t2, 0($t1)         # *t1 = 11111111; // set switches as input
     # set LED's as output:
     LA $t1, TRISE	   # int32* t1= &TRISE; //load TRISE address 
@@ -37,23 +43,23 @@ main:
     # turn leds off
     LA $t3, PORTE          # int32* t3 = &PORTE; //load PORTE address 
     SW $0, 0($t3)	   # *t3 = 4; // set TRISE to 0 to turn all LEDS off 
-    LI $t6,0b10000000	   # counter 8
-    LI $t1, 1
-    LI $t4, 0b1
+    LI $t6,0b10000000	   # counter 8 when to count backwards
+    LI $t1, 1		   # counter 1
+    LI $t4, 0b1            # first led
     # loop to turn led 1-8 on
     Loop1:
 	# get users input from switches
 	LW $a0,PORTD     # value of the input button 
 	ANDI $a0,$a0,0b111100000000 # mask for switches bit 8-11,
-	SRL $a0,$a0,8    # t2 >> 8; // shift the value of t2, 4 to right
+	SRL $a0,$a0,8    # a0 >> 8; // shift the value of a0, 4 to right, since switches start from 8
 	ANDI $a0,$a0,0b1111 # mask for only desired digits double checking
 	ADD $a0,$a0,1
 	JAL Calc # calculate the speed of the light
 	NOP 
- 	BEQ $t4,$t6,Loop2 # if t4 is 8, so the last LED turns on too
-	SW $t4,0($t3)	   # turn led[i] on
-	SLL $t4,$t4,1    # t2 << 1; // shift the value of t2, 4 to right
-	JAL Waste_Time
+ 	BEQ $t4,$t6,Loop2 # if t4 is 8, turn led's backward 8-1
+	SW $t4,0($t3)	   # turn led[i] on, 1-8
+	SLL $t4,$t4,1    # t4 << 1; // shift the value of t4 by one to get to the next led
+	JAL mydelay
 	NOP
 	J Loop1
 	NOP
@@ -62,24 +68,24 @@ main:
     	# get users input from switches
 	LW $a0,PORTD     # value of the input button 
 	ANDI $a0,$a0,0b111100000000 # mask for switches bit 8-11,
-	SRL $a0,$a0,8    # t2 >> 8; // shift the value of t2, 4 to right
+	SRL $a0,$a0,8    # a0 >> 8; // shift the value of a0, 4 to right, since switches start from 8
 	ANDI $a0,$a0,0b1111 # mask for only desired digits double checking
 	ADD $a0,$a0,1
 	JAL Calc # calculate the speed of the light
 	NOP 
- 	BEQ $t4,$t1,Loop1 # if t4 is 1, so the last LED turns on too
-	SW $t4,0($t3)	   # turn led[i] on
-	SRL $t4,$t4,1    # t2 >> 1; // shift the value of t2, 4 to right
-	JAL Waste_Time
+ 	BEQ $t4,$t1,Loop1 # if t4 is 1, turn led's backward 1-8
+	SW $t4,0($t3)	   # turn led[i] on, 8-1
+	SRL $t4,$t4,1    # t4 >> 1; // shift the value of t4 by one to get to the next led
+	JAL mydelay
 	NOP
 	J Loop2
 	NOP	
 Calc:
-    LW $t5, counter
+    LW $t5, speed
     MUL $t5,$t5,$a0
     JR $31
     NOP
-Waste_Time:
+mydelay:
     ADD $t7,$t5,$0 # copy t5 which is the calculated light speed
     Start_Loop:
 	SUBU $t7,$t7,1
@@ -96,6 +102,6 @@ endProgram:
     NOP
 .data    
 HelloWorld: .asciiz "Assembly Hello World \n"
-counter: .word 10000
+speed: .word 10000 # max light speed
     
     
